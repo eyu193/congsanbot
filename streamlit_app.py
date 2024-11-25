2d7eb33648f27a93cfd526774954bd2111a01cf9
eyu193 <110981054+eyu193@users.noreply.github.com>
2024-11-25 21:29:32 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,56 +1,26 @@
 import streamlit as st
-from openai import OpenAI
+import google.generativeai as genai
 
-# Show title and description.
-st.title("💬 Chatbot")
-st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
-)
+# Tạo header
+st.header('CONGSANMUONNAM🤖')
+st.markdown("I am HO CHI MINH Chatbot based on Genimi-Pro what can I help you today 😊")
+with st.sidebar:
+    API_input = st.text_input("Gemini API Key" , type="password")
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+try:
+    genai.configure(api_key= API_input)
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+    user_input = st.text_input("Type your question here and press Enter" , key="user_input")
 
-    # Create a session state variable to store the chat messages. This ensures that the
-    # messages persist across reruns.
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+    model = genai.GenerativeModel('gemini-pro')
 
-    # Display the existing chat messages via `st.chat_message`.
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
-
-        # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
-
-        # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
-
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        with st.chat_message("assistant"):
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+    if user_input:
+        if not API_input:
+            st.warning("Please enter your [API](https://ai.google.dev/) into sidebar.")
+        else:
+            response = model.generate_content(user_input)
+            for letters in respone.text:
+                st.write(letters)
+        
+except Exception:
+    st.error('Please enter the correct [API](https://ai.google.dev/) Key')
